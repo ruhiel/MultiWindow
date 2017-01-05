f9f1c0ce97c560d28e65374538b00c000fc6429d
ruhiel_murrue <ruhiel1983@gmail.com>
2017-01-05 21:33:55 +0900

    初回コミット

---
+++ MultiWindow/View/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using MultiWindow.View;
+using MultiWindow.ViewModel;
+using System.Windows;
+
+namespace MultiWindow
+{
+	/// <summary>
+	/// MainWindow.xaml の相互作用ロジック
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			DataContextChanged += (o, e) =>
+			{
+				var vm = DataContext as MainViewModel;
+				vm.OpenSubWindow += (sender, arg) =>
+				{
+					// サブウィンドウ表示
+					SubView wnd = new SubView();
+					wnd.Left = this.Left + this.Width;
+					wnd.DataContext = vm;
+					wnd.Show();
+
+					// サブウィンドウ表示
+					wnd = new SubView();
+					wnd.Left = this.Left + this.Width + 100;
+					wnd.DataContext = vm;
+					wnd.Show();
+				};
+			};
+
+			DataContext = new MainViewModel();
+		}
+	}
+}
---
+++ MultiWindow/ViewModel/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MultiWindow.ViewModel
+{
+	/// <summary>
+	/// ViewModel
+	/// </summary>
+	public class MainViewModel : ViewModelBase
+	{
+		public ICommand OpenSubWindowCommand { get; private set; }
+		public event EventHandler OpenSubWindow;
+
+		// プロパティの値を保持するメンバ変数
+		private bool _visibleproperty = true;
+
+		// プロパティ
+		public bool VisibleProperty
+		{
+			get { return _visibleproperty; }
+			set
+			{
+				// 値が変更されたら、INotifyPropertyChanged.PropertyChanged を発生させる。
+				if (_visibleproperty != value)
+				{
+					_visibleproperty = value;
+					RaisePropertyChanged(nameof(VisibleProperty));
+				}
+			}
+		}
+
+		public ICommand ChangeCommand { get; private set; }
+
+		// コンストラクタ
+		public MainViewModel()
+		{
+			ChangeCommand = CreateCommand(v => {
+				VisibleProperty = !VisibleProperty;
+			});
+
+			OpenSubWindowCommand = CreateCommand(v =>
+			{
+				OpenSubWindow?.Invoke(this, EventArgs.Empty);
+			});
+		}
+	}
+}
